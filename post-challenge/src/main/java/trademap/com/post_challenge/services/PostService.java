3f82517f34e15cadb97896c3164b68e8ffab3e61
Gabriel Soares <gabrielsoacc@gmail.com>
2024-10-24 21:40:55 -0300

    feat: created post service interface

--- post-challenge/src/main/java/trademap/com/post_challenge/services/PostService.java
+++ post-challenge/src/main/java/trademap/com/post_challenge/services/PostService.java
@@ -1,37 +1,11 @@
 package trademap.com.post_challenge.services;
 
-import org.springframework.stereotype.Service;
+import trademap.com.post_challenge.domain.entities.Post;
 
-import trademap.com.post_challenge.entities.Post;
-import trademap.com.post_challenge.repositories.PostRepository;
+public interface PostService {
 
-@Service
-public class PostService {
-    
-    private PostRepository postRepository;
-
-    public PostService(PostRepository postRepository){
-        this.postRepository = postRepository;
-    }
-
-    public Post createPost(String title, String description, String body){
-
-        if (title == null) throw new IllegalArgumentException("Title cannot be null");
-        if (title.isEmpty()) throw new IllegalArgumentException("Title cannot be empty");
-
-        if (description == null) throw new IllegalArgumentException("Description cannot be null");
-        if (description.isEmpty()) throw new IllegalArgumentException("Description cannot be empty");
-        
-        if (body == null) throw new IllegalArgumentException("Body cannot be null");
-        if (body.isEmpty()) throw new IllegalArgumentException("Body cannot be empty");
-
-        Post post = new Post(title, description, body);
-
-        postRepository.save(post);
-
-        return post;
-    }
-
-    
-
+    Post createPost(String title, String description, String body);
+    Post getPost(String id);
+    Post updatePost(String id, String title, String description, String body);
+    void deletePost(String id);   
 }
